4f880ad5ae0423a85ce8e8c8dbf4463dcc5910d8
ericcnguyen04 <ericc.win@gmail.com>
2023-02-20 22:07:24 -0500

    adding routes

---
+++ src/components/pages/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Navbar() {
+    return(
+        <div>
+            <div>Home</div>
+            <div>Projects</div>
+            <div>About me</div>
+            <div>Contact</div>
+            <div>Resume</div>
+        </div>
+    )
+}
