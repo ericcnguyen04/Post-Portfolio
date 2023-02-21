3626e3d5ff98cda524fec752705b6d67d774e9d8
ericcnguyen04 <ericc.win@gmail.com>
2023-02-21 14:39:10 -0500

    added two projects

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,7 +1,15 @@
 export default function Projects() {
     return(
-        <>
-            brrr
-        </>
+        <div>
+            <div>
+                Project 1
+                <a href="https://ericcnguyen04.github.io/PROJECT1/" target="_blank">Crazy Time</a>
+            </div>
+
+            <div>
+                Project 3
+                <a href="https://endearing-hummingbird-7ff714.netlify.app/" target="_blank">Mern Maveriks</a>
+            </div>      
+        </div>
     )
 }
