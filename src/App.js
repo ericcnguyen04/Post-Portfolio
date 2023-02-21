f695ce107d7ad46f7c92a49177023567cad85317
ericcnguyen04 <ericc.win@gmail.com>
2023-02-20 22:24:19 -0500

    nav functionable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 
-import Home from './components/pages/Home'
 import Navbar from './components/pages/Navbar';
+import Home from './components/pages/Home'
+import Projects from './components/pages/Projects';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Resume from './components/pages/Resume';
+
 import './App.css'
 
 function App() {
@@ -13,6 +18,26 @@ function App() {
         <Route 
           path="/"
           element={<Home />}
+        />
+
+        <Route 
+          path="/projects"
+          element={<Projects />}
+        />
+        
+        <Route 
+          path="/about"
+          element={<About />}
+        />
+
+        <Route 
+          path="/contact"
+          element={<Contact />}
+        />
+
+        <Route 
+          path="/resume"
+          element={<Resume />}
         />
 
       </Routes>
--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,13 +1,13 @@
-
+import {Link} from 'react-router-dom'
 
 export default function Navbar() {
     return(
         <div>
-            <div>Home</div>
-            <div>Projects</div>
-            <div>About me</div>
-            <div>Contact</div>
-            <div>Resume</div>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/projects'}>Projects</Link>
+            <Link to={'/about'}>About me</Link>
+            <Link to={'/contact'}>Contact</Link>
+            <Link to={'/resume'}>Resume</Link>
         </div>
     )
 }
