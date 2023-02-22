7929726f737541538c1d0fbd730017d7e25f6ae2
ericcnguyen04 <ericc.win@gmail.com>
2023-02-21 23:11:35 -0500

    all slides/ pages has content

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,8 +2,17 @@
 
 export default function About() {
     return(
-        <>
-        abt
-        </>
+        
+        <div>
+            <video src='public/video.mp4' width="750" height="500" controls>
+            </video>
+            
+            <div>
+            {/* my swim era */}
+            During my time as captain of my high school swim team, I learned a lot about what it takes to be a strong and effective leader. I was responsible for motivating my team, setting goals, and making decisions that would ultimately impact our performance. This required me to be a good communicator and a good listener, able to understand the needs and concerns of my team members while also keeping our larger goals in mind. I learned how to be adaptable and flexible, to think on my feet and respond quickly to changing situations. Most importantly, I learned that leadership is about more than just giving orders - it's about building relationships, fostering trust, and creating a sense of unity and purpose within a team. These skills have stayed with me throughout my life, and I know they will continue to serve me well as I pursue my goals and dreams.
+            </div>
+            {/*  */}
+ß
+        </div>
     )
 }
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -2,8 +2,26 @@
 
 export default function Contact() {
     return(
-        <>
-        stalker page
-        </>
+        <div>
+            <div>
+                Eric Nguyen <br />
+                Ericc.win@gmail.com <br />
+                (404) 933-7143 <br />
+                https://github.com/ericcnguyen04 <br />
+                https://www.linkedin.com/in/eric-nguyen-3970b0229/
+            </div>
+
+            <div>
+                <h1>
+                    Contact me!
+                </h1>
+            
+                {/* form to where recruiters can send me email */}
+            </div>
+
+            <div>
+                Located at Atlanta, Georgia
+            </div>
+        </div>
     )
 }
