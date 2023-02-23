2cd809384aa2faed9e7acbda4035684be2ef6545
ericcnguyen04 <ericc.win@gmail.com>
2023-02-22 20:02:46 -0500

    break

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,25 +3,31 @@
 export default function Contact() {
     return(
         <div>
-            <div>
-                Eric Nguyen <br />
-                Ericc.win@gmail.com <br />
-                (404) 933-7143 <br />
-                https://github.com/ericcnguyen04 <br />
-                https://www.linkedin.com/in/eric-nguyen-3970b0229/
-            </div>
+            <center>
 
-            <div>
-                <h1>
-                    Contact me!
-                </h1>
-            
-                {/* form to where recruiters can send me email */}
-            </div>
+                <div>
+                    <h1>
+                        Feel free to contact me!
+                    </h1>
+                
+                    {/* form to where recruiters can send me email */}
+                </div>
+                <div>
+                    <h3>
+                    Eric Nguyen <br />
+                    Ericc.win@gmail.com <br />
+                    (404) 933-7143 <br />
+                    <a href="https://github.com/ericcnguyen04">My Github</a> <br />
+                    <a href="https://www.linkedin.com/in/eric-nguyen-3970b0229/">My Linkedin</a>
+                    </h3>
+                </div>
 
-            <div>
-                Located at Atlanta, Georgia
-            </div>
+                
+
+                <div>
+                    Located at Atlanta, Georgia
+                </div>
+            </center>
         </div>
     )
 }
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,15 +1,32 @@
+import Card from 'react-bootstrap/Card';
+
 export default function Projects() {
     return(
         <div>
-            <div>
-                Project 1
-                <a href="https://ericcnguyen04.github.io/PROJECT1/" target="_blank">Crazy Time</a>
-            </div>
-
-            <div>
-                Project 3
-                <a href="https://endearing-hummingbird-7ff714.netlify.app/" target="_blank">Mern Maveriks</a>
-            </div>      
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title><h1>Crazy Time</h1></Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted"><h2>My first project</h2></Card.Subtitle>
+                    <Card.Text>
+                        My very first project consists of HTML, CSS, and JS. This project was inspired by a popular 2014 game known as, Flappy Bird
+                    </Card.Text>
+                    <Card.Link href="https://ericcnguyen04.github.io/PROJECT1/">Try it out!</Card.Link>
+                    <Card.Link href="https://github.com/ericcnguyen04/PROJECT1">My Github</Card.Link>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title><h1>Mern Maveriks</h1></Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted"><h2>Full Stack</h2></Card.Subtitle>
+                    <Card.Text>
+                        My first <strong>group</strong> project consists of React, mongoose, . This project was inspired by a popular 2014 game known as, Flappy Bird
+                    </Card.Text>
+                    <Card.Link href="https://endearing-hummingbird-7ff714.netlify.app/items">Try it out!</Card.Link>
+                    <Card.Link href="https://github.com/ericcnguyen04/project-3-server">My backend!</Card.Link>
+                    <Card.Link href="https://github.com/ericcnguyen04/project-3-client">My frontend!</Card.Link>
+                </Card.Body>
+            </Card>
+            
         </div>
     )
 }
