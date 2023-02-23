9347bc0fc3810e31a6553cb1348ea1976eabe478
ericcnguyen04 <ericc.win@gmail.com>
2023-02-22 21:14:07 -0500

    ready to deploy

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -2,8 +2,8 @@ import Card from 'react-bootstrap/Card';
 
 export default function Projects() {
     return(
-        <div>
-            <Card style={{ width: '18rem' }}>
+        <div className='project'>
+            <Card style={{ width: '20vw' }}>
                 <Card.Body>
                     <Card.Title><h1>Crazy Time</h1></Card.Title>
                     <Card.Subtitle className="mb-2 text-muted"><h2>My first project</h2></Card.Subtitle>
@@ -14,12 +14,12 @@ export default function Projects() {
                     <Card.Link href="https://github.com/ericcnguyen04/PROJECT1">My Github</Card.Link>
                 </Card.Body>
             </Card>
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '20vw' }}>
                 <Card.Body>
                     <Card.Title><h1>Mern Maveriks</h1></Card.Title>
                     <Card.Subtitle className="mb-2 text-muted"><h2>Full Stack</h2></Card.Subtitle>
                     <Card.Text>
-                        My first <strong>group</strong> project consists of React, mongoose, . This project was inspired by a popular 2014 game known as, Flappy Bird
+                        My first <strong>group</strong> project consists the usage of MERN stack . This project was inspired by a popular 2014 game known as, Flappy Bird
                     </Card.Text>
                     <Card.Link href="https://endearing-hummingbird-7ff714.netlify.app/items">Try it out!</Card.Link>
                     <Card.Link href="https://github.com/ericcnguyen04/project-3-server">My backend!</Card.Link>
