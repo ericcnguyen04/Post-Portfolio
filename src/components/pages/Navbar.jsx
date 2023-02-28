4dda2c1ab40249429453f994e54e7e57a8e945bd
ericcnguyen04 <ericc.win@gmail.com>
2023-02-27 19:00:18 -0500

    committing before any big changes

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,4 +1,8 @@
-import Link from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {Link} from 'react-router-dom'
+
 
 // css
 import '../../css/NavStyle.css'
@@ -7,12 +11,12 @@ export default function ColorSchemesExample() {
   return (
     <>
       <div className='linkContainer'>
-        <a href="/" className='links'>Eric Nguyen</a>
-        <a href="/" className='links'>Home</a>
-        <a href="/projects" className='links'>Projects</a>
-        <a href="/about" className='links'>About me</a>
-        <a href="/contact" className='links'>Contact</a>
-        <a href="/resume" className='links'>Resume</a>
+          <a href="/" className='links'>Eric Nguyen</a>
+          <a href="/" className='links'>Home</a>
+          <a href="/projects" className='links'>Projects</a>
+          <a href="/about" className='links'>About me</a>
+          <a href="/contact" className='links'>Contact</a>
+          <a href="/resume" className='links'>Resume</a>
       </div>
     </>
   )
