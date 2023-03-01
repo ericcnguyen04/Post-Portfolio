09506b94ac13904e19331a3bbe02e1101795ad14
ericcnguyen04 <ericc.win@gmail.com>
2023-02-28 21:45:03 -0500

    getting started

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -12,14 +12,14 @@ export default function Contact() {
                 
                     {/* form to where recruiters can send me email */}
                 </div>
-                <div>
-                    <h3>
-                    Eric Nguyen <br />
-                    Ericc.win@gmail.com <br />
-                    (404) 933-7143 <br />
+
+                <div className="contact-card">
+                    <h1>Eric Nguyen </h1>
+                    <h3>Ericc.win@gmail.com</h3>
+                    <h3>(404) 933-7143</h3>
                     <a href="https://github.com/ericcnguyen04">My Github</a> <br />
                     <a href="https://www.linkedin.com/in/eric-nguyen-3970b0229/">My Linkedin</a>
-                    </h3>
+                    
                 </div>
 
                 
