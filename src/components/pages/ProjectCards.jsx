e33bf7f2590224e3a13f0b108a4b43a1f2645708
ericcnguyen04 <ericc.win@gmail.com>
2023-03-02 01:02:57 -0500

    enough for today, need to work on image redirect

--- src/components/pages/ProjectCards.jsx
+++ src/components/pages/ProjectCards.jsx
@@ -2,15 +2,24 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 import { useNavigate } from 'react-router-dom';
+import { redirect } from 'react-router-dom';
 
 function BasicExample(props) {
 
     let navigate = useNavigate();
+    // const routeChange = () =>{ 
+    //     let path = props.deploy; 
+    //     console.log(path)
+    //     return(
+    //         redirect to={path} /
+    //     )
+    //   }
+
 
   return (
     <Card style={{ width: '20vw' }}> 
     {/* 20vw */}
-      <Card.Img variant="top" src={props.img} onClick={() => location.href}/>
+      <Card.Img variant="top" src={props.img} onClick={() => {console.log('sd')}}/>
       <Card.Body>
         <Card.Title><h1>{props.title}</h1></Card.Title>
         <Card.Subtitle className="mb-2 text-muted"><h2>{props.subtitle}</h2></Card.Subtitle>
