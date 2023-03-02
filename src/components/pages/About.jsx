8ffd0d399b4cbe4b69d3ae49d9d6fc8dcdd34a76
ericcnguyen04 <ericc.win@gmail.com>
2023-03-01 19:43:42 -0500

    rearranging conttentj

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,30 +1,12 @@
-import { useNavigate } from 'react-router-dom';
+
 
 import Button from 'react-bootstrap/Button';
 
 export default function About() {
 
-    let navigate = useNavigate();
 
     return(
         <div>  
-            <div>
-                <center><h1>Eric Nguyen</h1></center>
-            </div>
-
-            <div className="brand">
-                {/* brand statement */}
-                I am an aspiring full-stack developer with visions of passion and ambition! As I grew up, I always loved witnessing the end product of my projects, which included my efforts with front and back end, and I felt nothing but joy. I will work with my peers and create a positive environment by being compatible and ensuring that there is no confusion! Previously, as an insurance agent, I have habits of taking the initiative and making sure that we are making progress to promote time management. Most importantly, my mindset is always on a growth mindset and open to critique while providing feedback.
-
-                {/* redirect stating 'learn more about me */}
-                <div>
-                    <center>
-                    <Button variant="primary" onClick={() => navigate('/about')}>
-                        Learn more about me!</Button>{' '}
-                    </center>
-                </div>
-            </div>
-
             {/* how i get into software engineering */}
             <div>
                 <center>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,7 +9,22 @@ export default function Home() {
     
     return(
         <>
-            
+            <div>
+                <center><h1>Eric Nguyen</h1></center>
+            </div>
+
+            <div className="brand">
+                {/* brand statement */}
+                I am an aspiring full-stack developer with visions of passion and ambition! As I grew up, I always loved witnessing the end product of my projects, which included my efforts with front and back end, and I felt nothing but joy. I will work with my peers and create a positive environment by being compatible and ensuring that there is no confusion! Previously, as an insurance agent, I have habits of taking the initiative and making sure that we are making progress to promote time management. Most importantly, my mindset is always on a growth mindset and open to critique while providing feedback.
+
+                {/* redirect stating 'learn more about me */}
+                <div>
+                    <center>
+                    <Button variant="primary" onClick={() => navigate('/about')}>
+                        Learn more about me!</Button>{' '}
+                    </center>
+                </div>
+            </div>
         </>
     )
 }
