b2359255258c2c530fcd1fe36bf026d95b9f2195
ericcnguyen04 <ericc.win@gmail.com>
2023-04-02 04:12:39 -0400

    added animated background

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,28 @@
 import HomeContent from './HomeContent'
+import '../../App.css'
 
 export default function Home() {
-    
     return(
-        <div className='background'>
-            <center>
-                <h1>Welcome to Eric's Portfolio</h1>
-            </center>
-            <HomeContent />
+        <div>
+            <div class="context">
+                <h1>Pure Css Animated Background</h1>
+            </div>
+
+
+            <div class="area" >
+                <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div >
         </div>
     )
 }
