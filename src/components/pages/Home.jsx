6683f525993b650bc47f7d907837a5a7c7fecba0
ericcnguyen04 <ericc.win@gmail.com>
2023-04-02 04:25:28 -0400

    added my name, add brand later

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,7 +5,8 @@ export default function Home() {
     return(
         <div>
             <div class="context">
-                <h1>Pure Css Animated Background</h1>
+                <h1>Eric Nguyen</h1>
+                {/* <h3>an aspiring full-stack developer with visions of passion and ambition! As I grew up, I always loved witnessing the end product of my projects, which included my efforts with front and back end, and I felt nothing but joy. I will work with my peers and create a positive environment by being compatible and ensuring that there is no confusion! Previously, as an insurance agent, I have habits of taking the initiative and making sure that we are making progress to promote time management. Most importantly, my mindset is always on a growth mindset and open to critique while providing feedback.</h3> */}
             </div>
 
 
