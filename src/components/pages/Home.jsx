62783a8d23fd8f5419e32996bc19a6c4d03f5cd8
ericcnguyen04 <ericc.win@gmail.com>
2023-04-04 21:09:23 -0400

    about to make function

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,11 @@ import HomeContent from './HomeContent'
 import '../../App.css'
 
 export default function Home() {
+
+    const scrollTo = () => {
+        
+    }
+
     return(
         <div>
             
@@ -12,6 +17,9 @@ export default function Home() {
                 <div className='contextBio'>
                     <h3>an aspiring full-stack developer with promising contributions to create an impact</h3>
                     {/* <h3>an aspiring full-stack developer with visions of passion and ambition! As I grew up, I always loved witnessing the end product of my projects, which included my efforts with front and back end, and I felt nothing but joy. I will work with my peers and create a positive environment by being compatible and ensuring that there is no confusion! Previously, as an insurance agent, I have habits of taking the initiative and making sure that we are making progress to promote time management. Most importantly, my mindset is always on a growth mindset and open to critique while providing feedback.</h3> */}
+                </div>
+                <div className='navContainer'>
+                    <button className='button'>boom</button>
                 </div>
             </div>
 
