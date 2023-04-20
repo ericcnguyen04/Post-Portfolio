8fb536552eef8657509dd6ab6bb25c7bb0bbf17b
ericcnguyen04 <ericc.win@gmail.com>
2023-04-20 19:15:40 -0400

    contact card editinged

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -5,26 +5,11 @@ export default function Contact() {
         <div className='background'>
             <center>
 
-                <div>
-                    <h1>
-                        Feel free to contact me!
-                    </h1>
-                
-                    {/* form to where recruiters can send me email */}
-                </div>
-
                 <div className="contact-card">
-                    <h1>Eric Nguyen </h1>
                     <h3>Ericc.win@gmail.com</h3>
                     <h3>(404) 933-7143</h3>
                     <a href="https://github.com/ericcnguyen04">My Github</a> <br />
                     <a href="https://www.linkedin.com/in/eric-nguyen-3970b0229/">My Linkedin</a>
-                    
-                </div>
-
-                
-
-                <div>
                     Located at Atlanta, Georgia
                 </div>
             </center>
