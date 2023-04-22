ff64200a4064cd1367c97f4ffa329e32f1d21519
ericcnguyen04 <ericc.win@gmail.com>
2023-04-22 18:50:50 -0400

    margined the footer

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -6,11 +6,13 @@ export default function Contact() {
 
 
                 <div className="contact-card">
-                    <h3>Ericc.win@gmail.com</h3>
-                    <h3>(404) 933-7143</h3>
-                    <a href="https://github.com/ericcnguyen04">My Github</a> <br />
-                    <a href="https://www.linkedin.com/in/eric-nguyen-3970b0229/">My Linkedin</a>
-                    <h6>Located at Atlanta, Georgia</h6>
+                    <div className="contact-container">
+                        <h3>Ericc.win@gmail.com</h3>
+                        <h3>(404) 933-7143</h3>
+                        <a href="https://github.com/ericcnguyen04">My Github</a> <br />
+                        <a href="https://www.linkedin.com/in/eric-nguyen-3970b0229/">My Linkedin</a>
+                        <h6>Located at Atlanta, Georgia</h6>
+                    </div>
                 </div>
 
         </div>
