0815dab6b2f76140a755130bbf78cbe2b25149ac
ericcnguyen04 <ericc.win@gmail.com>
2023-04-22 19:04:03 -0400

    added logos

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -7,8 +7,10 @@ export default function Contact() {
 
                 <div className="contact-card">
                     <div className="contact-container">
-                        <h3>Ericc.win@gmail.com</h3>
-                        <h3>(404) 933-7143</h3>
+                        <h4>Ericc.win@gmail.com</h4>
+                        <h4>(404) 933-7143</h4>
+                        <a href="https://github.com/ericcnguyen04"><img src="/content/github.png"></img></a>
+                        <a href="https://www.linkedin.com/in/eric-nguyen-3970b0229/"><img src="/content/linkedin.png"></img></a>
                         <a href="https://github.com/ericcnguyen04">My Github</a> <br />
                         <a href="https://www.linkedin.com/in/eric-nguyen-3970b0229/">My Linkedin</a>
                         <h6>📍 Located at Atlanta, Georgia</h6>
