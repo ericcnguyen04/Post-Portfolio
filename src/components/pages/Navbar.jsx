1881a92d782abfca6a4a45095edabdf3fc01b72d
ericcnguyen04 <ericc.win@gmail.com>
2023-04-30 19:55:53 -0400

    atttempts

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,5 +1,5 @@
-import {Link} from 'react-scroll'
-
+import * as Scroll from 'react-scroll';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 // css
 import '../../css/NavStyle.css'
@@ -7,33 +7,33 @@ import '../../css/NavStyle.css'
 export default function Navbar() {
   return (
     <div>
-      <ul className='hidden sm:flex text-color'>
-                <li className='hover:underline underline-offset-4'>
-                    <Link to="home" smooth={true} duration={500}>
-                        home
-                    </Link>
-                </li>
-                <li className='hover:underline underline-offset-4'>
-                    <Link to="about" smooth={true} duration={500}>
-                        about
-                    </Link>
-                </li>
-                <li className='hover:underline underline-offset-4'>
-                    <Link to="skills" smooth={true} duration={500}>
-                        skills
-                    </Link>
-                </li>
-                <li className='hover:underline underline-offset-4'>
-                    <Link to="work" smooth={true} duration={500}>
-                        work
-                    </Link>
-                </li>
-                <li className='hover:underline underline-offset-4'>
-                    <Link to="contact" smooth={true} duration={500}>
-                        contact
-                    </Link>
-                </li>
-            </ul>
+        <ul>
+            <li>
+                <Link to="home" smooth={true} duration={500}>
+                    home
+                </Link>
+            </li>
+            <li>
+                <Link to="about" smooth={true} duration={500}>
+                    about
+                </Link>
+            </li>
+            <li>
+                <Link to="skills" smooth={true} duration={500}>
+                    skills
+                </Link>
+            </li>
+            <li>
+                <Link to="work" smooth={true} duration={500}>
+                    work
+                </Link>
+            </li>
+            <li>
+                <Link to="contact" smooth={true} duration={500}>
+                    contact
+                </Link>
+            </li>
+        </ul>
     </div>
   )
 }
