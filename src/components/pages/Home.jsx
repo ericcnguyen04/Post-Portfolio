8a03bcf5c5dbb0842cff9a558c39b8eb524a33a4
ericcnguyen04 <ericc.win@gmail.com>
2023-05-01 23:15:19 -0400

    remove cringe

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -26,7 +26,7 @@ export default function Home() {
                     <h3>an aspiring full-stack developer with promising contributions to create an impact</h3>
                     {/* <h3>an aspiring full-stack developer with visions of passion and ambition! As I grew up, I always loved witnessing the end product of my projects, which included my efforts with front and back end, and I felt nothing but joy. I will work with my peers and create a positive environment by being compatible and ensuring that there is no confusion! Previously, as an insurance agent, I have habits of taking the initiative and making sure that we are making progress to promote time management. Most importantly, my mindset is always on a growth mindset and open to critique while providing feedback.</h3> */}
                 </div>
-                <Navbar />
+                {/* <Navbar /> */}
             </div>
 
 
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -3,20 +3,21 @@ import Button from 'react-bootstrap/Button';
 export default function Resume() {
 
 	// Function will execute on click of button
-	const onButtonClick = () => {
-		// using Java Script method to get PDF file
-		fetch('ENSE resume.pdf').then(response => {
-			response.blob().then(blob => {
-				// Creating new object of PDF file
-				const fileURL = window.URL.createObjectURL(blob);
-				// Setting various property values
-				let alink = document.createElement('a');
-				alink.href = fileURL;
-				alink.download = 'ENSE resume.pdf';
-				alink.click();
-			})
-		})
-	}
+	// will download a file
+	// const onButtonClick = () => {
+	// 	// using Java Script method to get PDF file
+	// 	fetch('ENSE resume.pdf').then(response => {
+	// 		response.blob().then(blob => {
+	// 			// Creating new object of PDF file
+	// 			const fileURL = window.URL.createObjectURL(blob);
+	// 			// Setting various property values
+	// 			let alink = document.createElement('a');
+	// 			alink.href = fileURL;
+	// 			alink.download = 'ENSE resume.pdf';
+	// 			alink.click();
+	// 		})
+	// 	})
+	// }
 
     return (
         <div className='background'>
