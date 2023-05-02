2892cbb7547b1c57e03b992bce806cbc96e7334e
ericcnguyen04 <ericc.win@gmail.com>
2023-05-01 23:17:48 -0400

    added typewriter

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import '../../App.css'
 
 import {useEffect, useRef, useState} from 'react';
+import Typewriter from 'typewriter-effect'
 
 
 
@@ -23,6 +24,15 @@ export default function Home() {
                     <h1>Eric Nguyen</h1>
                 </div>
                 <div className='contextBio'>
+                    <Typewriter 
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString("I'm a Fullstack Software Engineer.")
+                                .pauseFor(200)
+                                .start();
+                        }}
+                    />
+                    
                     <h3>an aspiring full-stack developer with promising contributions to create an impact</h3>
                     {/* <h3>an aspiring full-stack developer with visions of passion and ambition! As I grew up, I always loved witnessing the end product of my projects, which included my efforts with front and back end, and I felt nothing but joy. I will work with my peers and create a positive environment by being compatible and ensuring that there is no confusion! Previously, as an insurance agent, I have habits of taking the initiative and making sure that we are making progress to promote time management. Most importantly, my mindset is always on a growth mindset and open to critique while providing feedback.</h3> */}
                 </div>
