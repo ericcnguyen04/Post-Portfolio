8e12f9580552ed3f41bfd91f1212786307d642b6
ericcnguyen04 <ericc.win@gmail.com>
2023-05-13 22:28:37 -0400

    dope asf contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -26,7 +26,7 @@ export default function Contact() {
                         </div>
                     </div>
                     <div>
-                        <div className="styled-input">
+                        <div className="styled-input" id="phoneNumber">
                             <input type="text" required />
                             <label>Phone Number</label> 
                         </div>
@@ -38,7 +38,7 @@ export default function Contact() {
                         </div>
                     </div>
                     <div>
-                        <div>Send Message</div>
+                        <div className='submit-btn'>Send Message</div>
                     </div>
                 {/* <a href="https://github.com/ericcnguyen04" target="_blank"><img src="/content/github.png"></img></a>
                 <a href="https://www.linkedin.com/in/eric-nguyen-3970b0229/" target="_blank"><img src="/content/linkedin.png"></img></a> */}
