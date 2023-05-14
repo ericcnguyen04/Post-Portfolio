c1a2670271d8da9ae24a5485348bde366d4af07b
ericcnguyen04 <ericc.win@gmail.com>
2023-05-13 21:19:35 -0400

    restroom break

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -5,39 +5,39 @@ export default function Contact() {
         <div className='contactContainer'>
             <div>
                 <div>
-                        <h1>contact us</h1>
+                    <h1>contact us</h1>
                 </div>
 	        <div>
 			    <h4>We'd love to hear from you!</h4>
 	        </div>
             <div>
+                <div>
                     <div>
-                        <div>
-                            <input type="text" required />
-                            <label>Name</label> 
-                        </div>
+                        <input type="text" required />
+                        <label>Name</label> 
                     </div>
+                </div>
+                <div>
                     <div>
-                        <div>
-                            <input type="text" required />
-                            <label>Email</label> 
-                        </div>
+                        <input type="text" required />
+                        <label>Email</label> 
                     </div>
+                </div>
+                <div>
                     <div>
-                        <div>
-                            <input type="text" required />
-                            <label>Phone Number</label> 
-                        </div>
+                        <input type="text" required />
+                        <label>Phone Number</label> 
                     </div>
+                </div>
+                <div>
                     <div>
-                        <div>
-                            <textarea required></textarea>
-                            <label>Message</label>
-                        </div>
+                        <textarea required></textarea>
+                        <label>Message</label>
                     </div>
-                    <div>
-                        <div>Send Message</div>
-                    </div>
+                </div>
+                <div>
+                    <div>Send Message</div>
+                </div>
             </div>
             </div>
         </div>
