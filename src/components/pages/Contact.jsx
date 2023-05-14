6fd8b071da3f03eb38b40db6f9fc64633108a0b3
ericcnguyen04 <ericc.win@gmail.com>
2023-05-14 19:42:32 -0400

    add and commit

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -5,6 +5,20 @@ import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+      
     return(
         <div className='contactContainer'>
             <div>
